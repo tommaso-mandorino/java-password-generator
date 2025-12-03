01322f1a95dc81b4c61b7d768354daa4d001bf7e
tommaso-mandorino <tommasomandorino@outlook.it>
2025-12-03 23:27:36 +0100

    feat: add password generation feature after acquiring user input

--- src/org/lessons/java/security/PasswordGenerator.java
+++ src/org/lessons/java/security/PasswordGenerator.java
@@ -1,11 +1,39 @@
 package org.lessons.java.security;
 
+import java.util.Scanner;
+
 public class PasswordGenerator {
 
     public static void main(String[] args) {
         
+        Scanner scanner = new Scanner(System.in);
 
-        
+        System.out.print("Inserisci il tuo nome: ");
+        String firstName = scanner.nextLine();
+
+        System.out.print("Inserisci il tuo cognome: ");
+        String lastName = scanner.nextLine();
+
+        System.out.print("Inserisci il tuo colore preferito: ");
+        String favoriteColor = scanner.nextLine();
+
+        System.out.print("Inserisci il tuo giorno di nascita: ");
+        int dayOfBirth = scanner.nextInt();
+
+        System.out.print("Inserisci il tuo mese di nascita: ");
+        int monthOfBirth = scanner.nextInt();
+
+        System.out.print("Inserisci il tuo anno di nascita: ");
+        int yearOfBirth = scanner.nextInt();
+
+        int dateSum = dayOfBirth + monthOfBirth + yearOfBirth;
+
+        String generatedPassword = '"' + firstName + '-' + lastName + '-' + favoriteColor + '-' + dateSum + '"';
+
+        System.out.println("La password generata per te è: " + generatedPassword);
+
+        scanner.close();
+
     }
 
 }
